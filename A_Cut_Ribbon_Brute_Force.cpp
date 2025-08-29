15926e413e1db0b51cdd34788e24071dc225db3d
Niloy Chandra Datta <niloycd98@gmail.com>
2025-08-29 20:29:48 +0600

    Add files via upload

--- A_Cut_Ribbon_Brute_Force.cpp
+++ A_Cut_Ribbon_Brute_Force.cpp
@@ -1,43 +1,33 @@
-// Recursion with Memoization
+// https://codeforces.com/contest/189/problem/A
+// Brute Force
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int rec(int len, int a, int b, int c, vector<int>& dp)
-{
-    if (len == 0)
-    {
-        return 0;
-    }
-    if (len < 0)
-    {
-        return -1e9;
-    }
-
-    if (dp[len] != -1)
-    {
-        return dp[len];
-    }
-
-    int p1 = 1 + rec(len - a, a, b, c, dp);
-    int p2 = 1 + rec(len - b, a, b, c, dp);
-    int p3 = 1 + rec(len - c, a, b, c, dp);
-
-    return dp[len] = max({p1, p2, p3});
-}
-
 void solve()
 {
-    int n, a, b, c;
-    cin >> n >> a >> b >> c;
+    int n, a, b, c; cin >> n >> a >> b >> c;
 
-    vector<int> dp(n + 1, -1);
+    int ans = 0;
 
-    int ans = rec(n, a, b, c, dp);
+    for (int i = 0; i * a <= n; ++i)
+    {
+        for (int j = 0; i * a + j * b <= n; ++j)
+        {
+            int rem = n - (i * a + j * b);
+
+            if (rem % c == 0)
+            {
+                int k = rem / c;
+                ans = max(ans, i + j + k);
+            }
+        }
+    }
 
     cout << ans << endl;
 }
+
 
 int32_t main()
 {
